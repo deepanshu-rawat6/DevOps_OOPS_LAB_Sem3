6efd038754576437b88357b80d18cfd18e8e4d98
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-08-28 21:53:26 +0530

    updating ranged sum and permutations

--- Lab_Experiments/Experiment 3/Permutations.java
+++ Lab_Experiments/Experiment 3/Permutations.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class Permutations {
     void compute(){
         Scanner sc=new Scanner(System.in);
-        int a,b,c;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        c=sc.nextInt();
         int[] arr=new int[3];
-        arr[0]=a;
-        arr[1]=b;
-        arr[2]=c;
+        arr[0]=sc.nextInt();
+        arr[1]=sc.nextInt();
+        arr[2]=sc.nextInt();
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                for(int k=0;k<3;k++){
+                    if(i!=j && j!=k && i!=k){
+                        System.out.println(arr[i]+""+arr[j]+""+arr[k]);
+                    }
+                }
+            }
+        }
     }
     public static void main(String[] args) {
         Permutations obj=new Permutations();
--- Lab_Experiments/Experiment 3/Ranged_SUm.java
+++ Lab_Experiments/Experiment 3/Ranged_SUm.java
@@ -1,16 +1,46 @@
 
 public class Ranged_SUm {
-    void compute(){
+    int compute(){
         int i=45;
         int sum=0;
         while(i<250){
             sum+=i;
             i+=5;
         }
-        System.out.println("The sum of all integers greater than 40 and less than 250 that are divisible by 5: "+sum);
+        return sum;
+//        System.out.println("The sum of all integers greater than 40 and less than 250 that are divisible by 5: "+sum);
+    }
+    int calc(int x){
+        if(x==40) return 0;
+        return x+calc(x-5);
     }
     public static void main(String[] args) {
         Ranged_SUm obj=new Ranged_SUm();
-        obj.compute();
+        long start = System.nanoTime();
+
+        // call the method
+        int ans=obj.calc(245);
+        // get the end time
+        long end = System.nanoTime();
+
+        // execution time
+        long execution = end - start;
+//        obj.compute();
+//        System.out.println(obj.calc(245));
+        long start1 = System.nanoTime();
+
+        // call the method
+        int t=obj.compute();
+
+        // get the end time
+        long end1 = System.nanoTime();
+
+        // execution time
+        long execution1 = end1 - start1;
+        System.out.println("Execution time: " + execution + " nanoseconds");
+        System.out.println("Execution time: " + execution1 + " nanoseconds");
+
+        System.out.println("Output:"+ans);
+
     }
 }
