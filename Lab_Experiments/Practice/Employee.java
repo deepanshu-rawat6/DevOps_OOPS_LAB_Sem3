707e9f57e18c98b065beb3028886bf03a7b88ff7
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-08-31 11:36:16 +0530

    adding practice question

---
+++ Lab_Experiments/Practice/Employee.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Employee {
+    String name;
+    int JS;
+    int BS;
+    Employee(String n,int bs,int js){
+        name=n;
+        JS=js;
+        BS=bs;
+    }
+    double calc_Gross(int sal){
+//        System.out.println(this.BS);
+        double hra=(0.1)*(this.BS);
+        double da=(0.5)*(this.BS);
+        double ta=(0.05)*(this.BS);
+        return (this.BS)+hra+da+ta+sal;
+    }
+    double calc_Gross(){
+        double ta=(0.05)*BS;
+        return BS+ta;
+    }
+    void calc_netsal(){
+        double GS=0;
+        if(JS==1){
+//            Permanent
+            GS=calc_Gross(5000);
+        }else{
+//            Probation
+            GS=calc_Gross();
+        }
+        double it=calc_IT(GS);
+        double pf=(0.12)*GS;
+        double Net_sal=GS-it-pf;
+        printing(Net_sal);
+    }
+    double calc_IT(double gs){
+        if(gs>100000) return (0.3)*gs;
+        else if(gs>=50000) return (0.1)*gs;
+        else return 0;
+    }
+    void printing(double NS){
+        System.out.println();
+        System.out.println("Employee Name: "+name);
+        if(JS==1){
+            System.out.println("Permanent");
+        }else{
+            System.out.println("Probation");
+        }
+        System.out.println("Basic Salary: "+BS);
+        System.out.println("Net Salary: "+NS);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Employee Name:");
+        String n=sc.nextLine();
+        System.out.print("Base Salary:");
+        int bs=sc.nextInt();
+        System.out.print("Enter 1 for Permanent or 2 for Probation:");
+        int j=sc.nextInt();
+        Employee obj=new Employee(n,bs,j);
+        obj.calc_netsal();
+    }
+}
