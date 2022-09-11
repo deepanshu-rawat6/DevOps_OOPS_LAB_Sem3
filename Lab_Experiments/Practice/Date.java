2f44bca6ef335072511c23dbcc8c094a7c2e42bc
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-09-11 23:13:44 +0530

    adding quick sort

---
+++ Lab_Experiments/Practice/Date.java
@@ -0,0 +1,11 @@
+public class Date {
+    Date(){
+
+    }
+    public static void main(String[] args) {
+        Date obj=new Date();
+        int d=Integer.parseInt(args[0]);
+        int m=Integer.parseInt(args[1]);
+        int y=Integer.parseInt(args[2]);
+    }
+}
