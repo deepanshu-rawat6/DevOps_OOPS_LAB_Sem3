4d33ffb6e92743b3a65bdc76ca78e3d3a0b04a55
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-09-14 12:46:35 +0530

    adding codes of experiment 4 and 5

--- Lab_Experiments/Experiment 5/Maths.java
+++ Lab_Experiments/Experiment 5/Maths.java
@@ -2,7 +2,7 @@ interface Cal{
     float div(int a,int b);
     int mod(int a,int b);
 }
-public class Math implements Cal {
+public class Maths implements Cal {
     @Override
     public float div(int a, int b) {
         return (float)a/b;
@@ -14,7 +14,7 @@ public class Math implements Cal {
     }
 
     public static void main(String[] args) {
-        Math obj=new Math();
+        Maths obj=new Maths();
         System.out.println(obj.div(20,5));
         System.out.println(obj.mod(5,4));
     }
