e85e463d8238dfbe3ecbf1774b1b6c79f6e0cc62
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-10-19 00:31:14 +0530

    adding a few changes

--- Lab_Experiments/Experiment 4/Player.java
+++ Lab_Experiments/Experiment 4/Player.java
@@ -2,12 +2,10 @@ class Player {
     String name;
     String sport;
     Player(String name,String sport){
-        this.name=name;
-        this.sport=sport;
-    }
-    void display() {
+        this.name = name;
+        this.sport = sport;
         System.out.println("Player Name:" + this.name);
-        System.out.println("Sport Name:" + this.sport+"\n");
+        System.out.println("Sport Name:" + this.sport + "\n");
     }
 }
 class Cricket_Player extends Player{
@@ -29,10 +27,9 @@ class Hockey_Player extends Player{
 class Program{
     public static void main(String[] args) {
         Cricket_Player ob1=new Cricket_Player("MSD","Cricket");
+
         Football_Player ob2=new Football_Player("CR7","Football");
+
         Hockey_Player ob3=new Hockey_Player("XYZ","Hockey");
-        ob1.display();
-        ob2.display();
-        ob3.display();
     }
 }
--- Lab_Experiments/Experiment 4/PrivateMember.java
+++ Lab_Experiments/Experiment 4/PrivateMember.java
@@ -13,10 +13,11 @@ class Check extends PrivateMember{
     }
     void printing(){
         System.out.println("This is inside derived class");
+//        Cannot be accessed because private members are visible only to inside class in which they are declared
 //        System.out.println("Var:"+super.a);
     }
     public static void main(String[] args) {
-        Check obj=new Check();
+        Check obj = new Check();
        obj.display();
        obj.printing();
     }
